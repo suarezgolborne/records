b8a1bd3317eba563be79fd9d022c2492c07e2946
Sebastian Suarez Golborne <sebastian.suarez.golborne@svt.se>
2021-04-10 20:18:51 +0200

    anim

--- src/App.js
+++ src/App.js
@@ -7,31 +7,32 @@ import { SpotifyAuth, Scopes } from "react-spotify-auth";
 import "react-spotify-auth/dist/index.css";
 import { getAverageColor } from "fast-average-color-node";
 import { BgTint, BgImage, CoverImage } from "./App.styled";
+import { AnimatePresence } from "framer-motion";
 
 const {
   REACT_APP_CLIENT_ID,
   REACT_APP_REDIRECT_URI,
   REACT_APP_SPOTIFY_LIST,
-} = process.env; //TODO add playlist
+} = process.env;
 
 const App = () => {
   const [token, setToken] = useState();
   const [device, setDevice] = useState();
   const [position, setPosition] = useState(0);
-  const [isPlaying, setPlaying] = useState(false);
+  const [isPlaying, setPlaying] = useState();
 
-  const [currentAlbum, setCurrentAlbum] = useState(null);
-  const [currentAlbumChartPosition, setCurrentAlbumChartPosition] = useState(
-    null
-  );
-  const [currentAlbumCover, setCurrentAlbumCover] = useState(null);
-  const [bgImage, setBgImage] = useState(null);
-  const [bgColor, setBgColor] = useState(null);
+  const [currentAlbum, setCurrentAlbum] = useState();
+  const [currentAlbumChartPosition, setCurrentAlbumChartPosition] = useState();
+  const [currentAlbumCover, setCurrentAlbumCover] = useState();
+  const [bgImage, setBgImage] = useState();
+  const [bgColor, setBgColor] = useState();
 
-  const [nextAlbum, setNextAlbum] = useState(null);
-  const [nextAlbumChartPosition, setNextAlbumChartPosition] = useState(null);
-  const [nextAlbumCover, setNextAlbumCover] = useState(null);
-  const [nextAlbumBgImage, setNextAlbumBg] = useState(null);
+  const [nextAlbum, setNextAlbum] = useState();
+  const [nextAlbumChartPosition, setNextAlbumChartPosition] = useState();
+  const [nextAlbumCover, setNextAlbumCover] = useState();
+  const [nextAlbumBgImage, setNextAlbumBg] = useState();
+  const [nextBgColor, setNextBgColor] = useState();
+  const [previousBgColor, setPreviousBgColor] = useState();
 
   const [totalAlbums, setTotalAlbums] = useState("");
 
@@ -42,6 +43,27 @@ const App = () => {
   useEffect(() => {
     setToken(Cookies.get("spotifyAuthToken"));
   }, []);
+
+  const getBgColor = (src, callback, outputFormat) => {
+    var img = new Image();
+    img.crossOrigin = "Anonymous";
+    img.onload = function () {
+      var canvas = document.createElement("CANVAS");
+      var ctx = canvas.getContext("2d");
+      var dataURL;
+      canvas.height = this.naturalHeight;
+      canvas.width = this.naturalWidth;
+      ctx.drawImage(this, 0, 0);
+      dataURL = canvas.toDataURL(outputFormat);
+      callback(dataURL);
+    };
+    img.src = src;
+    if (img.complete || img.complete === undefined) {
+      img.src =
+        "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+      img.src = src;
+    }
+  };
 
   // get devices and first album
   useEffect(() => {
@@ -69,10 +91,10 @@ const App = () => {
         if (err) console.error(err);
         else {
           setTotalAlbums(data.tracks.total);
-          const offset = Math.floor(Math.random() * data.tracks.total);
-          const nextOffset = Math.floor(Math.random() * data.tracks.total);
+          let offset = Math.floor(Math.random() * data.tracks.total);
+          let nextOffset = Math.floor(Math.random() * data.tracks.total);
           setCurrentAlbumChartPosition(offset);
-          setNextAlbumChartPosition(nextOffset);
+          // setNextAlbumChartPosition(nextOffset);
 
           s.getPlaylistTracks(
             REACT_APP_SPOTIFY_LIST,
@@ -85,6 +107,17 @@ const App = () => {
               else {
                 setCurrentAlbum(data.items[0].track);
                 setCurrentAlbumCover(data.items[0].track.album.images[0].url);
+
+                getBgColor(
+                  data.items[0].track.album.images[0].url,
+                  function (dataUrl) {
+                    getAverageColor(dataUrl).then((color) => {
+                      setBgColor(
+                        `rgba(${color.value[0]},${color.value[1]},${color.value[2]},0.6)`
+                      );
+                    });
+                  }
+                );
 
                 s.getArtist(
                   data.items[0].track.artists[0].id,
@@ -110,6 +143,19 @@ const App = () => {
                       const img = new Image();
                       img.src = data.items[0].track.album.images[0].url;
                       setNextAlbumCover(img.src);
+
+                      getBgColor(
+                        data.items[0].track.album.images[0].url,
+                        function (dataUrl) {
+                          getAverageColor(dataUrl).then((color) => {
+                            setNextBgColor(
+                              `rgba(${color.value[0]},${color.value[1]},${color.value[2]},0.6)`
+                            );
+                            setPreviousBgColor(bgColor);
+                          });
+                        }
+                      );
+
                       s.getArtist(
                         data.items[0].track.artists[0].id,
                         function (err, data) {
@@ -129,36 +175,6 @@ const App = () => {
       });
     }
   }, [token]);
-
-  // average color
-  useEffect(() => {
-    function toDataURL(src, callback, outputFormat) {
-      var img = new Image();
-      img.crossOrigin = "Anonymous";
-      img.onload = function () {
-        var canvas = document.createElement("CANVAS");
-        var ctx = canvas.getContext("2d");
-        var dataURL;
-        canvas.height = this.naturalHeight;
-        canvas.width = this.naturalWidth;
-        ctx.drawImage(this, 0, 0);
-        dataURL = canvas.toDataURL(outputFormat);
-        callback(dataURL);
-      };
-      img.src = src;
-      if (img.complete || img.complete === undefined) {
-        img.src =
-          "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
-        img.src = src;
-      }
-    }
-
-    toDataURL(currentAlbum?.album.images[0].url, function (dataUrl) {
-      getAverageColor(dataUrl).then((color) => {
-        setBgColor(color.value);
-      });
-    });
-  }, [currentAlbum?.album.images]);
 
   const startAlbum = (position, device, currentAlbum) => {
     const PlayParameterObject = {
@@ -204,6 +220,7 @@ const App = () => {
     setCurrentAlbum(nextAlbum);
     setCurrentAlbumChartPosition(nextAlbumChartPosition);
     setCurrentAlbumCover(nextAlbumCover);
+    setBgColor(nextBgColor);
     setBgImage(nextAlbumBgImage);
     setPlaying(false);
     setPosition(0);
@@ -224,11 +241,22 @@ const App = () => {
           const img = new Image();
           img.src = data.items[0].track.album.images[0].url;
           setNextAlbumCover(img.src);
+          getBgColor(
+            data.items[0].track.album.images[0].url,
+            function (dataUrl) {
+              getAverageColor(dataUrl).then((color) => {
+                setNextBgColor(
+                  `rgba(${color.value[0]},${color.value[1]},${color.value[2]},0.6)`
+                );
+                setPreviousBgColor(bgColor);
+              });
+            }
+          );
 
           s.getArtist(data.items[0].track.artists[0].id, function (err, data) {
             if (err) console.error(err);
             else {
-              data.images && setNextAlbumBg(data.images[0].url);
+              data.images[0]?.url && setNextAlbumBg(data.images[0].url);
             }
           });
         }
@@ -237,7 +265,7 @@ const App = () => {
   };
 
   // Animation options
-  const variants = {
+  const variantsCover = {
     enter: () => {
       return {
         y: -1000,
@@ -259,12 +287,82 @@ const App = () => {
     },
   };
 
+  const variantsBgTint = bgColor && {
+    enter: {
+      opacity: 0,
+    },
+    center: {
+      opacity: 1,
+    },
+    exit: {
+      opacity: 0,
+    },
+  };
+
+  // const variantsBgTint = bgColor &&
+  //   nextBgColor && {
+  //     enter: () => {
+  //       return {
+  //         backgroundColor: `rgba(${nextBgColor[0]},${nextBgColor[1]},${nextBgColor[2]},0.6)`,
+  //       };
+  //     },
+  //     center: {
+  //       backgroundColor: `rgba(${bgColor[0]},${bgColor[1]},${bgColor[2]},0.6)`,
+  //     },
+  //     exit: () => {
+  //       return {
+  //         backgroundColor: `rgba(${bgColor[0]},${bgColor[1]},${bgColor[2]},0.6)`,
+  //       };
+  //     },
+  //   };
+
+  // initial="enter"
+  // exit="exit"
+  // bgColor={bgColor}
+
+  console.log(bgColor);
+
   return (
     <div className="app">
       {token ? (
         <>
-          <BgImage BgImage={bgImage} />
-          <BgTint bgColor={bgColor} />
+          {/* <motion.div
+          layoutId="outline"
+          className="outline"
+          initial={false}
+          animate={{ backgroundColor: bgColor }}
+          transition={{
+            backgroundColor: { duration: 2.4 },
+          }}
+        /> */}
+
+          {/* <BgTint
+            key={`${currentAlbumChartPosition}${bgColor}`}
+            initial={false}
+            variants={variantsBgTint}
+            animate="animate"
+            enter="enter"
+            exit="exit"
+            transition={{
+              backgroundColor: { duration: 2.4 },
+            }}
+          /> */}
+          <AnimatePresence initial={false}>
+            <BgImage
+              BgImage={bgImage}
+              key={`${currentAlbumChartPosition}${bgColor}`}
+              variants={variantsBgTint}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={{
+                x: { type: "spring", stiffness: 300, damping: 30 },
+                opacity: { duration: 0.6 },
+              }}
+            />
+            <BgTint bgColor={bgColor} />
+          </AnimatePresence>
+
           <div className="controls">
             {isPlaying ? (
               <div className="button" onClick={() => pauseAlbum()}>
@@ -292,13 +390,13 @@ const App = () => {
             <CoverImage
               src={currentAlbumCover}
               key={currentAlbumChartPosition}
-              variants={variants}
+              variants={variantsCover}
               initial="enter"
               animate="center"
               exit="exit"
               transition={{
                 y: { type: "spring", stiffness: 250, damping: 25 },
-                opacity: { duration: 0.7 },
+                opacity: { duration: 0.9 },
               }}
             />
 
