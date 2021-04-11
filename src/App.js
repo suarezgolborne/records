f92d01af0799b940e3b14426f0c8c60c84a9f61f
Sebastian Suarez Golborne <sebastian.suarez.golborne@svt.se>
2021-04-11 18:39:18 +0200

    so much

--- src/App.js
+++ src/App.js
@@ -8,15 +8,19 @@ import "react-spotify-auth/dist/index.css";
 import { getAverageColor } from "fast-average-color-node";
 import { BgTint, BgImage, CoverImage, LoginWrapper } from "./App.styled";
 import { AnimatePresence } from "framer-motion";
+// import SpotifyWebApi from "spotify-web-api-node";
+import request from "request";
 
 const {
   REACT_APP_CLIENT_ID,
   REACT_APP_REDIRECT_URI,
   REACT_APP_SPOTIFY_LIST,
+  REACT_APP_CLIENT_SECRET,
 } = process.env;
 
 const App = () => {
-  const [token, setToken] = useState();
+  const [token, setToken] = useState(null);
+  const [hasScopes, setHasScopes] = useState(false);
   const [device, setDevice] = useState();
   const [position, setPosition] = useState(0);
   const [isPlaying, setPlaying] = useState();
@@ -35,11 +39,41 @@ const App = () => {
   const [nextBgColor, setNextBgColor] = useState();
 
   const s = new spotifyApi();
-  s.setAccessToken(token);
+  // s.setAccessToken(token);
 
-  // set token
   useEffect(() => {
-    setToken(Cookies.get("spotifyAuthToken"));
+    if (!Cookies.get("spotifyAuthToken")) {
+      var client_id = REACT_APP_CLIENT_ID;
+      var client_secret = REACT_APP_CLIENT_SECRET;
+
+      var authOptions = {
+        url: "https://accounts.spotify.com/api/token",
+        headers: {
+          Authorization:
+            "Basic " +
+            new Buffer(client_id + ":" + client_secret).toString("base64"),
+        },
+        form: {
+          grant_type: "client_credentials",
+        },
+        json: true,
+      };
+
+      request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          // var token = body.access_token;
+          setToken(body.access_token);
+          s.setAccessToken(body.access_token);
+          setHasScopes(false);
+        }
+      });
+    } else {
+      // s.setAccessToken(Cookies.get("spotifyAuthToken"));
+      setHasScopes(true);
+      setToken(Cookies.get("spotifyAuthToken"));
+      s.setAccessToken(Cookies.get("spotifyAuthToken"));
+      window.history.pushState("", document.title, "/");
+    }
   }, []);
 
   const getBgColor = (src, callback, outputFormat) => {
@@ -63,44 +97,31 @@ const App = () => {
     }
   };
 
+  const persistentTrackOffset = (offset) => {
+    !hasScopes
+      ? Cookies.set("persistentTrackOffset", offset)
+      : Cookies.remove("persistentTrackOffset");
+  };
+
   // get devices and first album
   useEffect(() => {
     if (token) {
-      // window.history.replaceState("", "", "");
-      window.history.pushState("", document.title, window.location.pathname);
-      s.getMyDevices(function (err, data) {
-        if (err) console.error(err);
-        else {
-          let activeDevice = data.devices.find((device) => device.is_active);
-          let inactiveDevice = data.devices.find((device) => !device.is_active);
-
-          // sort by type and by active status?
-          if (data.devices.length > 0) {
-            if (activeDevice?.is_active) {
-              setDevice(activeDevice.id);
-              // s.transferMyPlayback([activeDevice.id]);
-            } else {
-              setDevice(inactiveDevice.id);
-              s.transferMyPlayback([inactiveDevice.id]);
-            }
-          }
-        }
-      });
-
       s.getPlaylist(REACT_APP_SPOTIFY_LIST, function (err, data) {
         if (err) console.error(err);
         else {
           setTotalAlbums(data.tracks.total);
           let offset = Math.floor(Math.random() * data.tracks.total);
           let nextOffset = Math.floor(Math.random() * data.tracks.total);
-          setCurrentAlbumChartPosition(offset);
+          setCurrentAlbumChartPosition(
+            parseInt(Cookies.get("persistentTrackOffset")) || offset
+          );
           setNextAlbumChartPosition(nextOffset);
 
           s.getPlaylistTracks(
             REACT_APP_SPOTIFY_LIST,
             {
               limit: 1,
-              offset: offset,
+              offset: Cookies.get("persistentTrackOffset") || offset,
             },
             function (err, data) {
               if (err) console.error(err);
@@ -108,6 +129,8 @@ const App = () => {
                 setCurrentAlbum(data.items[0].track);
                 setCurrentAlbumCover(data.items[0].track.album.images[0].url);
 
+                persistentTrackOffset(data.offset);
+                setPlaying(false);
                 getBgColor(
                   data.items[0].track.album.images[0].url,
                   function (dataUrl) {
@@ -173,25 +196,75 @@ const App = () => {
           );
         }
       });
+
+      // setToken(Cookies.get("spotifyAuthToken"));
+      // setHasScopes(true);
     }
   }, [token]);
 
   const startAlbum = (position, device, currentAlbum) => {
-    const PlayParameterObject = {
-      context_uri: currentAlbum?.album.external_urls.spotify,
-      position_ms: position,
-      device_id: device,
-    };
+    if (!device) {
+      s.getMyDevices(function (err, data) {
+        if (err) console.error(err);
+        else {
+          let activeDevice = data.devices.find((device) => device.is_active);
+          let inactiveDevice = data.devices.find((device) => !device.is_active);
 
-    if (device) {
+          // sort by type and by active status?
+          if (data.devices.length > 0) {
+            if (activeDevice?.is_active) {
+              setDevice(activeDevice.id);
+              s.transferMyPlayback([activeDevice.id]);
+
+              const PlayParameterObject = {
+                context_uri: currentAlbum?.album.external_urls.spotify,
+                position_ms: position,
+                device_id: activeDevice.id,
+              };
+
+              s.play(PlayParameterObject, function (err, data) {
+                if (err) console.error(err);
+                else {
+                  setPlaying(true);
+                }
+              });
+            } else {
+              setDevice(inactiveDevice.id);
+              s.transferMyPlayback([inactiveDevice.id]);
+
+              const PlayParameterObject = {
+                context_uri: currentAlbum?.album.external_urls.spotify,
+                position_ms: position,
+                device_id: inactiveDevice.id,
+              };
+
+              s.play(PlayParameterObject, function (err, data) {
+                if (err) console.error(err);
+                else {
+                  setPlaying(true);
+                }
+              });
+            }
+          } else {
+            window.open(currentAlbum?.album.external_urls.spotify, "name");
+
+            // setDevice(inactiveDevice.id);
+            // s.transferMyPlayback([inactiveDevice.id]);
+          }
+        }
+      });
+    } else {
+      const PlayParameterObject = {
+        context_uri: currentAlbum?.album.external_urls.spotify,
+        position_ms: position,
+        device_id: device,
+      };
       s.play(PlayParameterObject, function (err, data) {
         if (err) console.error(err);
         else {
           setPlaying(true);
         }
       });
-    } else {
-      window.open(currentAlbum?.album.external_urls.spotify, "name");
     }
 
     return;
@@ -225,6 +298,8 @@ const App = () => {
     setPlaying(false);
     setPosition(0);
 
+    persistentTrackOffset(nextAlbumChartPosition);
+
     // queue next album
     const nextOffset = Math.floor(Math.random() * totalAlbums);
     setNextAlbumChartPosition(nextOffset);
@@ -241,6 +316,7 @@ const App = () => {
           const img = new Image();
           img.src = data.items[0].track.album.images[0].url;
           setNextAlbumCover(img.src);
+
           getBgColor(
             data.items[0].track.album.images[0].url,
             function (dataUrl) {
@@ -297,123 +373,121 @@ const App = () => {
 
   return (
     <div className="app">
-      {token ? (
-        <>
-          <AnimatePresence initial={false}>
-            <BgImage
-              BgImage={bgImage}
-              key={`${currentAlbumChartPosition}${bgColor}`}
-              variants={variantsBgTint}
-              initial="enter"
-              animate="center"
-              exit="exit"
-              transition={{
-                opacity: { duration: 0.3 },
-              }}
-            />
-          </AnimatePresence>
-          <AnimatePresence initial={false}>
-            <BgTint
-              key={`${currentAlbumChartPosition}${bgColor}`}
-              variants={variantsBgTint}
-              initial="enter"
-              animate="center"
-              exit="exit"
-              bgColor={bgColor}
-              transition={{
-                opacity: { duration: 0.6 },
-              }}
-            />
-          </AnimatePresence>
+      <>
+        <AnimatePresence initial={false}>
+          <BgImage
+            BgImage={bgImage}
+            key={`${currentAlbumChartPosition}${bgColor}`}
+            variants={variantsBgTint}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{
+              opacity: { duration: 0.3 },
+            }}
+          />
+        </AnimatePresence>
+        <AnimatePresence initial={false}>
+          <BgTint
+            key={`${currentAlbumChartPosition}${bgColor}`}
+            variants={variantsBgTint}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            bgColor={bgColor}
+            transition={{
+              opacity: { duration: 0.6 },
+            }}
+          />
+        </AnimatePresence>
 
-          <div className="controls">
-            {isPlaying ? (
-              <div className="button" onClick={() => pauseAlbum()}>
-                <Pause
-                  color={"#ffffff"}
-                  title={"Pausa"}
-                  height="50px"
-                  width="50px"
-                />
-              </div>
-            ) : (
-              <div
-                className="button"
-                onClick={() => startAlbum(position, device, currentAlbum)}
-              >
-                <Play
-                  color={"#ffffff"}
-                  title={"Starta"}
-                  height="50px"
-                  width="50px"
-                />
-              </div>
-            )}
-
-            <CoverImage
-              src={currentAlbumCover}
-              key={currentAlbumChartPosition}
-              variants={variantsCover}
-              initial="enter"
-              animate="center"
-              exit="exit"
-              transition={{
-                y: { type: "spring", stiffness: 300, damping: 25 },
-                delay: 3.5,
-              }}
-            />
-
-            <div className="button" onClick={() => shuffleAlbum(totalAlbums)}>
-              <Dice
-                color={"#ffffff"}
-                title={"Slumpa ett nytt album"}
-                height="50px"
-                width="50px"
+        <div className="controls">
+          {hasScopes ? (
+            <>
+              {isPlaying ? (
+                <div className="button" onClick={() => pauseAlbum()}>
+                  <Pause
+                    color={"#ffffff"}
+                    title={"Pausa"}
+                    height="50px"
+                    width="50px"
+                  />
+                </div>
+              ) : (
+                <div
+                  className="button"
+                  onClick={() => startAlbum(position, device, currentAlbum)}
+                >
+                  <Play
+                    color={"#ffffff"}
+                    title={"Starta"}
+                    height="50px"
+                    width="50px"
+                  />
+                </div>
+              )}
+            </>
+          ) : (
+            <LoginWrapper>
+              <SpotifyAuth
+                redirectUri={REACT_APP_REDIRECT_URI}
+                clientID={REACT_APP_CLIENT_ID}
+                title={""}
+                logoClassName={"login"}
+                btnClassName={"btnlogin"}
+                scopes={[
+                  Scopes.playlistReadPrivate,
+                  Scopes.userModifyPlaybackState,
+                  Scopes.userReadCurrentlyPlaying,
+                  Scopes.userReadPlaybackState,
+                ]}
+                onClick={() => setHasScopes(true)}
               />
-            </div>
-          </div>
-          <div className="headerBlock">
-            <span className="heading">{`Världens ${totalAlbums} bästa skivor`}</span>
-            <span className="subHeading">
-              {currentAlbumChartPosition &&
-                `Nummer ${currentAlbumChartPosition}`}
-            </span>
-          </div>
-          {currentAlbum && (
-            <div className="albumTitleBlock">
-              <span className="heading">{currentAlbum?.artists[0].name}</span>
-              <span className="subHeading">{`${
-                currentAlbum?.album.name
-              } (${new Date(
-                currentAlbum?.album.release_date
-              ).getFullYear()})`}</span>
-            </div>
+            </LoginWrapper>
           )}
-        </>
-      ) : (
-        // Display the login page
-        <>
-          {!currentAlbum && (
-            <div className="header">
-              <h1>{`Sebastian Suarez-Golbornes ${totalAlbums} bästa skivor`}</h1>
-            </div>
-          )}
+          <CoverImage
+            src={currentAlbumCover}
+            key={currentAlbumChartPosition}
+            variants={variantsCover}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{
+              y: { type: "spring", stiffness: 300, damping: 25 },
+              delay: 3.5,
+            }}
+          />
 
-          <LoginWrapper>
-            <SpotifyAuth
-              redirectUri={REACT_APP_REDIRECT_URI}
-              clientID={REACT_APP_CLIENT_ID}
-              title={"Fortsätt med Spotify "}
-              scopes={[
-                Scopes.playlistReadPrivate,
-                Scopes.userModifyPlaybackState,
-                Scopes.userReadCurrentlyPlaying,
-                Scopes.userReadPlaybackState,
-              ]} // either style will work
+          <div className="button" onClick={() => shuffleAlbum(totalAlbums)}>
+            <Dice
+              color={"#ffffff"}
+              title={"Slumpa ett nytt album"}
+              height="50px"
+              width="50px"
             />
-          </LoginWrapper>
-        </>
-      )}
+          </div>
+        </div>
+        <div className="headerBlock">
+          <span className="heading">{`Världens ${totalAlbums} bästa skivor`}</span>
+          <span className="subHeading">
+            {currentAlbumChartPosition &&
+              `Nummer ${
+                parseInt(Cookies.get("persistentTrackOffset")) + 1 ||
+                currentAlbumChartPosition + 1
+              }`}
+          </span>
+        </div>
+        {currentAlbum && (
+          <div className="albumTitleBlock">
+            <span className="heading">{currentAlbum?.artists[0].name}</span>
+            <span className="subHeading">{`${
+              currentAlbum?.album.name
+            } (${new Date(
+              currentAlbum?.album.release_date
+            ).getFullYear()})`}</span>
+          </div>
+        )}
+      </>
     </div>
   );
 };
